d345dde3cae089bfdb50155ce2110799caf8b5c9
sidmehta111 <114196641+sidmehta111@users.noreply.github.com>
2023-09-13 11:50:54 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ module "irsa-ebs-csi" {
 resource "aws_eks_addon" "ebs-csi" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.20.0-eksbuild.1"
+  addon_version            = "v1.22.0-eksbuild.2"
   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
   tags = {
     "eks_addon" = "ebs-csi"
