9874aa133ff4c959b72175e1eecbc0cff53cda4e
sidmehta111 <114196641+sidmehta111@users.noreply.github.com>
2024-03-12 17:09:55 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,9 +23,8 @@ resource "random_string" "suffix" {
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.5.0"
-  tags = {
-  Name = "devops-vpc-${random_string.suffix.result}"
-  }
+  name = "devops-vpc-${random_string.suffix.result}"
+  
   cidr = "10.0.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
